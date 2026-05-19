829e1720c76faab5168921fd1d7cfee5cdfb2e83
Thomas Price <twprice@ncsu.edu>
2026-05-19 11:22:45 -0400

    Proper error output in CLI; error serialization

--- src/cli/App.ts
+++ src/cli/App.ts
@@ -1,14 +1,27 @@
 import readline from "readline";
 import { PS2 } from "../index";
 
+// Simplified an error so it no longer contains object references
+// This avoid possible issues with circular references when we try to serialize the result to JSON
+// Objects are simply converted to strings simply (not JSON stringified) to keep it minimal
+function simplifyError(error: any[]): string {
+    return error.map(item => {
+        return String(item);
+    }).join(" ");
+}
+
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 rl.on("line", line => {
     try {
         const rows = JSON.parse(line);
         const result = PS2.createEditList(rows);
-        console.log(JSON.stringify(result)); // flushes result as one line
+        const simplifiedResult = {
+            ...result,
+            errors: result.errors.map(simplifyError)
+        };
+        console.log(JSON.stringify(simplifiedResult)); // flushes result as one line
     } catch (e) {
-        console.error(JSON.stringify({
+        console.log(JSON.stringify({
             error: "Failed to build edit list",
             details: e instanceof Error ? e.message : String(e)
         }));
